4e5383f8b270f0b4173eaf448ff1abb842c5005b
ariana armijos <ariana.armijos2406@outlook.com>
2024-11-18 20:37:19 -0500

    apli1

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ComponentsModule } from '../../components/components.module';
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline' ,
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ];
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit() {}
+
+}
