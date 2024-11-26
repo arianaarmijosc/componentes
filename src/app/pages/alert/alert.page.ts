b821d50aeb206e2757edc3d1d7b58f24b7dfd240
ArianaArmijos24 <ariananatyarmijos2003@gmail.com>
2024-11-25 21:06:36 -0500

    Fin sección 8

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
   styleUrls: ['./alert.page.scss'],
 })
-export class AlertPage implements OnInit {
+export class AlertPage implements OnInit{
 
-  constructor() { }
 
-  ngOnInit() {
+  /*
+  componentes: Componente [] = [
+    {
+      icon: 'american-football-outline' ,
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ];
+
+  */
+
+  handlerMessage = '';
+  roleMessage = '';
+
+
+
+  constructor(private alertCtrl: AlertController) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+    backdropDismiss: false,
+    header: 'Alert',
+    subHeader: 'Important message',
+    message: 'This is an alert!',
+    buttons: ['OK']
+  });
+  await alert.present();
+  };
+  
+
+  async presentAlertMultilpeButton() {
+    const alert = await this.alertCtrl.create({
+    backdropDismiss: false,
+    header: 'Alert',
+    subHeader: 'Important message',
+    message: 'This is an alert!',
+    buttons: ['Cancel', 'Opne Modal', 'Delete']
+    });
+    await alert.present();
+    };
+
+    
+    /*
+    async presentAlertMultilpeButtonAction() {
+      const alert = await this.alertCtrl.create({
+      backdropDismiss: false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: [
+      {
+      text: 'OK!',
+      handler: () => {
+      console.log('Click en OK!')
+      }
+      },
+      
+      {
+      text: 'Cancelar',
+      role: 'cancel',
+      cssClass: 'rojo'
+      }
+      ]
+      });
+      await alert.present();
+      };
+    */
+
 
 }
